863c75dab830b9ecd37e8337fe270464fda6dc73
Cata ♡ <73033739+zupzupp@users.noreply.github.com>
2024-11-30 20:42:47 +0000

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen! uwu</h2>
+      <h2>Start editing to see some magic happen! :D </h2>
     </div>
   );
 }
